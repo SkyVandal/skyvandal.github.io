4cb39de6916d0d1e3d1506bd95c956189a7060d1
SkyVandal <bruno.codinha@tecnico.ulisboa.pt>
2020-01-28 12:57:54 +0000

    audio

--- main.js
+++ main.js
@@ -5,21 +5,6 @@ var key_sounds = ["sounds/key1.mp3", "sounds/key2.mp3", "sounds/key3.mp3", "soun
 var sound;
 var ind;
 
-function sound(src) {
-	this.sound = document.createElement("audio");
-	this.sound.src = src;
-	this.sound.setAttribute("preload", "auto");
-	this.sound.setAttribute("controls", "none");
-	this.sound.style.display = "none";
-	document.body.appendChild(this.sound);
-	this.play = function(){
-	  this.sound.play();
-	}
-	this.stop = function(){
-	  this.sound.pause();
-	}
-  }
-
 
 function resetInterval() {
 	title.innerText = text 
@@ -36,7 +21,7 @@ function resetInterval() {
 			return
 		}
 		ind = Math.floor(Math.random() * 5);
-		sound = new sound(key_sounds[ind]);
+		sound = new Audio(key_sounds[ind]);
 		sound.play();
 		title.innerText += text[i++]
 	}, 100)
